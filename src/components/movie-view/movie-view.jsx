276caec9b9b66f8b6d21440689aff29b0ecebe82
paulinemarg <paulinemargossian@gmail.com>
2021-08-04 20:16:30 +0200

    Create movie-view.jsx

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export class MovieView extends React.Component {
+
+  render() {
+    const { movie, onBackClick } = this.props;
+
+    return (
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.ImagePath} />
+        </div>
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director}</span>
+        </div>
+        <div className="movie-stars">
+          <span className="label">Stars: </span>
+          <span className="value">{movie.Stars}</span>
+        </div>
+        <div className="movie-releaseYear">
+          <span className="label">ReleaseYear: </span>
+          <span className="value">{movie.ReleaseYear}</span>
+        </div>
+        <div className="movie-rating">
+          <span className="label">Rating: </span>
+          <span className="value">{movie.Rating}</span>
+        </div>
+        <button onClick={() => { onBackClick(null); }}>Back</button>
+
+      </div>
+    );
+  }
+}
