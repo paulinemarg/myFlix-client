0867ce1427ce9f47704e1cce33e9be3604b674a3
Pauline <85729525+paulinemarg@users.noreply.github.com>
2021-08-09 08:55:24 +0200

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -10,7 +10,7 @@ export function RegistrationView(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password, email, birthday);
-    props.onRegister(register);
+    props.onRegister(username);
   };
 
   return (
