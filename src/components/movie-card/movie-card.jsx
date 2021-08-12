ef9e7695bab8edec4e0c1e0d0faa12b57c3dc9b8
paulinemarg <paulinemargossian@gmail.com>
2021-08-12 22:37:18 +0300

    Update movie-card.jsx
    added bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Badge, Card, Button } from 'react-bootstrap';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
 
     return (
-      <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
+      <Card className="h-100 shadow-sm bg-white rounded">
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body className="d-flex flex-column">
+          <div className="d-flex mb-2 justify-content-between">
+            <Card.Title className="mb-0 font-weight-bold">{movie.Title}</Card.Title>
+            <Badge bg="danger">{movie.Rating}</Badge>
+          </div>
+          <Card.Text className="text-secondary">{movie.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} className="mt-auto font-weight-bold" variant="dark">Open</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
@@ -14,6 +25,7 @@ export class MovieCard extends React.Component {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
