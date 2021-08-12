6a569deb6faa9156cf5b668bd224191d0e7a38ba
paulinemarg <paulinemargossian@gmail.com>
2021-08-12 22:37:50 +0300

    Update movie-view.jsx
    added Bootstrap

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import { Row, Col, Button } from 'react-bootstrap';
+
+import "./movie-view.scss";
+
 
 export class MovieView extends React.Component {
 
@@ -7,41 +11,42 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director}</span>
-        </div>
-        <div className="movie-stars">
-          <span className="label">Stars: </span>
-          <span className="value">{movie.Stars}</span>
-        </div>
-        <div className="movie-releaseYear">
-          <span className="label">ReleaseYear: </span>
-          <span className="value">{movie.ReleaseYear}</span>
-        </div>
-        <div className="movie-rating">
-          <span className="label">Rating: </span>
-          <span className="value">{movie.Rating}</span>
-        </div>
-        <button onClick={() => { onBackClick(); }}>Back</button>
-
-      </div>
+      <Row className="movie-view">
+        <Col xs={12} md={6} className="movie-poster">
+          <img className="w-75" class="img-fluid" src={movie.ImagePath} thumbnail />
+        </Col>
+        <Col xs={12} md={6} className="movie-body">
+          <div className="movie-title">
+            <span className="label"></span>
+            <h1 className="value">{movie.Title}</h1>
+          </div>
+          <div className="movie-description">
+            <span className="label"></span>
+            <span className="value">{movie.Description}</span>
+          </div>
+          <div className="movie-genre">
+            <span className="label"></span>
+            <h2 className="value">{movie.Genre}</h2>
+          </div>
+          <div className="movie-director">
+            <span className="label"></span>
+            <h2 className="value">{movie.Director}</h2>
+          </div>
+          <div className="movie-stars">
+            <span className="label"></span>
+            <h2 className="value">{movie.Stars}</h2>
+          </div>
+          <div className="movie-releaseYear">
+            <span className="label"> </span>
+            <h3 className="value">{movie.ReleaseYear}</h3>
+          </div>
+          <div className="movie-rating">
+            <span className="label"></span>
+            <h3 className="value">{movie.Rating}</h3>
+          </div>
+          <Button className="backbtn" variant="dark" onClick={() => { onBackClick(); }}>Go Back</Button>
+        </Col>
+      </Row>
     );
   }
 }
@@ -54,7 +59,7 @@ MovieView.propTypes = {
     Director: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired,
     Stars: PropTypes.string.isRequired,
-    ReleaseYear: PropTypes.string.isRequired,
-    Rating: PropTypes.string.isRequired,
+    ReleaseYear: PropTypes.date,
+    Rating: PropTypes.string,
   }).isRequired,
 };
