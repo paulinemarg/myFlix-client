d2f0d1e996ed3c206aa9c7ddff3d1c31fcda8512
paulinemarg <paulinemargossian@gmail.com>
2021-08-17 20:28:37 +0200

    Update movie-card.jsx
    add link + remove onMovieClick

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Badge, Card, Button } from 'react-bootstrap';
 
+import { Link } from "react-router-dom";
+
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
 
     return (
       <Card className="h-100 shadow-sm bg-white rounded">
-        <Card.Img variant="top" src={movie.ImagePath} />
+        <Link to={`/movies/${movie._id}`}>
+          <Card.Img variant="top" src={movie.ImagePath} />
+        </Link>
         <Card.Body className="d-flex flex-column">
-          <div className="d-flex mb-2 justify-content-between">
-            <Card.Title className="mb-0 font-weight-bold">{movie.Title}</Card.Title>
-            <Badge bg="danger">{movie.Rating}</Badge>
-          </div>
-          <Card.Text className="text-secondary">{movie.Description}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} className="mt-auto font-weight-bold" variant="dark">Open</Button>
+          <Link to={`/movies/${movie._id}`}>
+            <div className="d-flex mb-2 justify-content-between">
+              <Card.Title className="mb-0 font-weight-bold">{movie.Title}</Card.Title>
+              <Badge bg="danger">{movie.Rating}</Badge>
+            </div>
+            <Card.Text className="text-secondary">{movie.Description}</Card.Text>
+          </Link>
+          <Button className="mt-auto font-weight-bold" variant="dark">Open</Button>
         </Card.Body>
       </Card>
     );
