b76fe444ec9c39ba51ae4de6aafd9bfc81856bf5
paulinemarg <paulinemargossian@gmail.com>
2021-08-19 14:29:50 +0200

    Update profile-view.jsx
    displayed user info + migrated remove fav to movie-view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import { Row, Col, Form, Button, Card, FloatingLabel, Accordion } from 'react-bootstrap';
 
 import './profile-view.scss'
-import AccordionHeader from 'react-bootstrap/esm/AccordionHeader';
 
 export class ProfileView extends React.Component {
   constructor() {
@@ -24,14 +23,13 @@ export class ProfileView extends React.Component {
     const accessToken = localStorage.getItem('token');
     if (accessToken !== null) {
       this.getUser(accessToken);
-      this.getUserFavs(accessToken);
     }
   }
 
   // Gets user
   getUser(token) {
     const username = localStorage.getItem('user');
-    axios.get('https://backend-myflix.herokuapp.com/users/${username}', {
+    axios.get(`https:backend-myflix.herokuapp.com/users/${username}`, {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then((response) => {
@@ -47,34 +45,21 @@ export class ProfileView extends React.Component {
       });
   }
 
-  // Gets fav
-  getUserFavs(token) {
-    axios.get('https://backend-myflix.herokuapp.com/users/' + localStorage.getItem('user'),
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((response) => {
-        this.setState({
-          FavoriteMovies: response.data.FavoriteMovies,
-        });
-      });
-  }
-
-  // remove Favourite Movie
   removeFavouriteMovie() {
-    const token = localStorage.getItem("token");
-    const username = localStorage.getItem("user");
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('user');
 
-    axios.delete(`https://backend-myflix.herokuapp.com/users/${username}/movies/${movie._id}`, {
+
+    axios.delete(`https:backend-myflix.herokuapp.com/users/${username}/movies/${movie._id}`, {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then(() => {
-        alert("Removed!");
+        alert('Movie is removed from favorites!');
         this.componentDidMount();
       })
       .catch(function (error) {
         console.log(error);
-      });
+      })
   }
 
   handleUpdate(e, newUsername, newPassword, newEmail, newBirthday) {
@@ -96,7 +81,7 @@ export class ProfileView extends React.Component {
     const token = localStorage.getItem("token");
     const username = localStorage.getItem("user");
 
-    axios.put(`https://backend-myflix.herokuapp.com/users/${username}`, {
+    axios.put(`https:backend-myflix.herokuapp.com/users/${username}`, {
       headers: { Authorization: `Bearer ${token}` },
       data: {
         Username: newUsername ? newUsername : this.state.Username,
@@ -111,7 +96,7 @@ export class ProfileView extends React.Component {
           Username: response.data.Username,
           Password: response.data.Password,
           Email: response.data.Email,
-          Birthday: response.data.Birth,
+          Birthday: response.data.Birthday,
         });
         localStorage.setItem("user", this.state.Username);
         window.open(`/users/${username}`, "_self");
@@ -121,35 +106,35 @@ export class ProfileView extends React.Component {
       });
   }
   setUsername(input) {
-    this.Username = input;
+    this.state.Username = input;
   }
 
   setPassword(input) {
-    this.Password = input;
+    this.state.Password = input;
   }
 
   setEmail(input) {
-    this.Email = input;
+    this.state.Email = input;
   }
 
   setBirthday(input) {
-    this.Birthday = input;
+    this.state.Birthday = input;
   }
 
   handleDeleteUser(e) {
     e.preventDefault();
 
-    const token = localStorage.getItem("token");
-    const username = localStorage.getItem("user");
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('user');
 
-    axios.delete(`https://backend-myflix.herokuapp.com/users/${username}`, {
+    axios.delete(`https:backend-myflix.herokuapp.com/users/${username}`, {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then(() => {
-        localStorage.removeItem("user");
-        localStorage.removeItem("token");
-        alert("Your account has been removed.");
-        window.open(`/`, "_self");
+        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        alert('Your account has been deleted.');
+        window.open(`/`, '_self');
       })
       .catch((e) => {
         console.log(e);
@@ -168,35 +153,33 @@ export class ProfileView extends React.Component {
               <h3 className="text-dark">My Profile</h3>
             </Accordion.Header>
             <Accordion.Body className="full-white w-100">
-              <Form noValidate className="w-75 my-auto" validated={validated} onSubmit={(e) =>
-                this.handleUpdate(e, this.Username, this.Password, this.Email, this.Birth
-                )}>
+              <Form noValidate validated={validated} className="update-form" onSubmit={(e) => this.handleUpdate(e, newUsername, newPassword, newEmail, newBirthday)}>
                 <Row>
                   <Form.Group className="mb-3" controlId="formBasicUsername">
                     <FloatingLabel controlId="username" label="Username">
-                      <Form.Control type="username" placeholder="Username" onChange={(e) => this.setUsername(e.target.value)} />
+                      <Form.Control type="text" value={this.state.Username} onChange={(e) => this.setState({ Username: e.target.value })} />
                     </FloatingLabel>
                   </Form.Group>
                   <Col>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                       <FloatingLabel controlId="password" label="Password">
-                        <Form.Control type="password" placeholder="Password" onChange={(e) => this.setPassword(e.target.value)} />
+                        <Form.Control type="password" value={this.state.Password} onChange={(e) => this.setState({ Password: e.target.value })} />
                       </FloatingLabel>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                       <FloatingLabel controlId="email" label="Email address">
-                        <Form.Control type="email" placeholder="Email address" onChange={(e) => this.setEmail(e.target.value)} />
+                        <Form.Control type="email" value={this.state.Email} onChange={(e) => this.setState({ Email: e.target.value })} />
                       </FloatingLabel>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicBirthday">
                       <FloatingLabel controlId="birthday" label="Birthday">
-                        <Form.Control type="date" onChange={(e) => this.setBirthday(e.target.value)} />{" "}
+                        <Form.Control type="date" value={this.state.Birthday} onChange={(e) => this.setState({ Birthday: e.target.value })} />
                       </FloatingLabel>
                     </Form.Group>
                   </Col>
                 </Row>
                 <Card.Body>
-                  <Button className="mx-3 mt-2" variant="outline-light" type="submit">Update</Button>
+                  <Button className="mx-3 mt-2" type="submit" variant="outline-light">Update</Button>
                   <Button className="mx-3 mt-2" variant="outline-danger" onClick={(e) => this.handleDeleteUser(e)}>Delete</Button>
                 </Card.Body>
               </Form>
@@ -207,29 +190,23 @@ export class ProfileView extends React.Component {
               <h3 className="m-auto black-text">Favorites</h3>
             </Accordion.Header>
             <Accordion.Body className="text-center full-black" sm={12} md={6}>
-              {FavoriteMovies.length === 0 && (
-                <div className="text-center text-light m-auto">
-                  You don`t have favorite movies yet!
-                </div>
-              )}
-              <div className="d-flex justify-content-center ">
-                {movies.map((movie) => {
-                  if (
-                    movie._id ===
-                    FavoriteMovies.find((m) => m._id === movies._id)
-                  ) {
-                    return (
-                      <Col className="text-center justify-content-center">
-                        <Row className="text-dark" key={movie._id}>
-                          <Col className="m-auto image-container-profile" sm={12} md={6} lg={5}>
-                            <img className="w-75 m-auto mt-2" key={movie._id} src={movie.ImagePath} />
-                            <Button className="remove-favorite w-50 px-6 m-auto mt-2 custom-remove" variant="danger" value={movie._id} onClick={(e) => this.removeFavouriteMovie(e, movie)}>Remove</Button>
-                          </Col>
-                        </Row>
-                      </Col>
-                    );
-                  }
-                })}
+              {FavoriteMovies.length === 0 && <div className="text-center text-light m-auto">You don`t have favorite movies yet!</div>}
+              <div className="favorite-movies d-flex justify-content-center ">
+                {FavoriteMovies.length > 0 &&
+                  movies.map((movie) => {
+                    if (movie._id === FavoriteMovies.find((favoriteMovie) => favoriteMovie === movie._id)) {
+                      return (
+                        <Col className="text-center justify-content-center">
+                          <Row className="text-dark" key={movie._id}>
+                            <Col className="m-auto image-container-profile" sm={12} md={6} lg={5}>
+                              <img className="w-75 m-auto mt-2" className="movieCard" src={movie.ImagePath} />
+                            </Col>
+                            <Button size='sm' className='profile-button remove-favorite' variant='danger' value={movie._id} onClick={(e) => this.removeFavouriteMovie(e, movie)}>Remove</Button>
+                          </Row>
+                        </Col>
+                      );
+                    }
+                  })}
               </div>
             </Accordion.Body>
           </Accordion.Item>
