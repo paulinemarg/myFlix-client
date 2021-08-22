679fefe5ab7df69fcd80495502e6579899f18884
paulinemarg <paulinemargossian@gmail.com>
2021-08-22 17:38:14 +0200

    Update profile-view.jsx
    added classname + 2 svg for update and delete account

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -167,8 +167,8 @@ export class ProfileView extends React.Component {
       <Row className="profile-view d-flex ">
         <Accordion defaultActiveKey="0" className="custom-accordion">
           <Accordion.Item style={{ backgroundColor: 'black' }} eventKey="0">
-            <Accordion.Header>
-              <h3 className="text-dark">My Profile</h3>
+            <Accordion.Header className="custom-header">
+              <h3 className="profile text-dark">My Profile</h3>
             </Accordion.Header>
             <Accordion.Body className="full-white w-100">
               <Form noValidate validated={validated} className="update-form" onSubmit={(e) => this.handleUpdate(e)}>
@@ -197,8 +197,12 @@ export class ProfileView extends React.Component {
                   </Col>
                 </Row>
                 <Card.Body>
-                  <Button className="mx-3 mt-2" type="submit" variant="outline-light">Update</Button>
-                  <Button className="mx-3 mt-2" variant="outline-danger" onClick={(e) => this.handleDeleteUser(e)}>Delete</Button>
+                  <Button className="mx-3 mt-2" type="submit" variant="outline-light"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                    <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                  </svg>Update</Button>
+                  <Button className="mx-3 mt-2" variant="outline-danger" onClick={(e) => this.handleDeleteUser(e)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                  </svg>Delete Account</Button>
                 </Card.Body>
               </Form>
             </Accordion.Body>
