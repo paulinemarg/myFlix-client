b67a62ffe37380e7894f8d6c9f89e2190099b9a0
paulinemarg <paulinemargossian@gmail.com>
2021-08-25 19:48:15 +0200

    Update reducers.js
    removed update user action

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import { SET_FILTER, SET_MOVIES, SET_USER, UPDATE_USER } from '../actions/actions';
+import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
 
 function visibilityFilter(state = '', action) {
   switch (action.type) {
@@ -23,9 +23,6 @@ function movies(state = [], action) {
 function user(state = '', action) {
   switch (action.type) {
     case SET_USER:
-      return action.value
-
-    case UPDATE_USER:
       return action.value;
     default:
       return state;
