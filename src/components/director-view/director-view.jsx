6ae3987276cac634d4377c1e90b8ddf733855df9
paulinemarg <paulinemargossian@gmail.com>
2021-12-19 16:25:58 +0100

    changed director view styling

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -8,22 +8,20 @@ export class DirectorView extends React.Component {
   render() {
     const { director } = this.props;
     return (
-      <Container className="director-container mt-5">
-        <Row className="director-image">
-          <Col xs={12}>
+      <Container fluid className="director-container">
+        <Row className="director">
+          <Col className="text-center">
             <img className="director-img" src={director.Image}></img>
           </Col>
           <Row className="director-view">
-            <span className="label"></span>
-            <span className="value text-white font-weight-bold">{director.Name}-{director.Birth}</span>
+            <span className="value font-weight-bold">{director.Name}-{director.Birth}</span>
           </Row>
           <Row className="director-bio">
-            <span className="label"></span>
-            <span className="value text-white">{director.Bio}</span>
+            <span className="value text-center text-white">{director.Bio}</span>
           </Row>
           <Row className="director-filmography">
-            <span className="label text-white font-weight-bold">His famous works:</span>
-            <span className="value text-white">{director.Filmography}</span>
+            <span className="label font-weight-bold">His famous works:</span>
+            <span className="value text-center text-white">{director.Filmography}</span>
           </Row>
         </Row>
       </Container>
